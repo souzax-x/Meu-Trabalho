649ced9ae4745ece6780b95f67459a6a8009bb04
souzax-x <jn.alvesouza@hotmail.com>
2024-09-09 09:08:30 -0300

    att service

---
+++ service-work.js
@@ -0,0 +1,66 @@
+const CACHE_NAME = 'paises-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html', // O arquivo HTML principal
+  '/css/bootstrap.css', // Estilos CSS
+  '/css/style.css', // Estilos CSS
+  '/script.js', // Seu script JS principal
+  'https://kit.fontawesome.com/1c98dd5571.js', // FontAwesome
+  'https://code.jquery.com/jquery-3.7.1.min.js', // jQuery
+  'https://unpkg.com/axios@1.6.7/dist/axios.min.js' // Axios
+];
+
+// Instalando o service worker e adicionando recursos ao cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceptando requisições e retornando recursos do cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Recurso encontrado no cache
+        if (response) {
+          return response;
+        }
+        // Recurso não encontrado no cache, buscar da rede
+        return fetch(event.request);
+      })
+  );
+});
+
+// Atualizando o service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+          console.log('Service Worker registrado com sucesso:', registration);
+        })
+        .catch(error => {
+          console.error('Falha ao registrar o Service Worker:', error);
+        });
+    });
+  }
+  
